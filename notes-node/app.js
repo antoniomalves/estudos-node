0cd7d48b04f6adc5d8663edc5a2b03f790d46843
Antonio M Alves Lima <antonio.malves.lima@gmail.com>
2018-08-04 10:29:18 -0300

    importando modulos criados no sistema e utilizando variaveis existentes nesse modulo.

--- notes-node/app.js
+++ notes-node/app.js
@@ -2,7 +2,8 @@ console.log('Starting app')
 
 const fs = require('fs')
 const os = require('os')
+const notes = require('./notes')
 
 let user = os.userInfo()
 
-fs.appendFileSync('greetings.txt', `Hello ${user.username}`)
+fs.appendFileSync('greetings.txt', `Hello ${user.username}! You are ${notes.age}\n`)
