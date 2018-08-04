ff76b9ddca05d8c71461e92d39df95066fd4a6b1
Antonio M Alves Lima <antonio.malves.lima@gmail.com>
2018-08-04 12:16:40 -0300

    retirando do projeto arquivos que não são mais necessários.

--- notes-node/app.js
+++ notes-node/app.js
@@ -1,11 +1,6 @@
 console.log('Starting app')
 
 const fs = require('fs')  
-const os = require('os')
 const _ = require('lodash')
+
 const notes = require('./notes')
-
-console.log(_.isString('Antonio'))
-
-let filteredArray = _.uniq(['Antonio', 1, 'Antonio', 1, 2, 3, 4])
-console.log(filteredArray)
