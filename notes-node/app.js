f9ef82c17d7f0aca1f9f82c13ba62db60d071036
Antonio M Alves Lima <antonio.malves.lima@gmail.com>
2018-08-03 23:30:17 -0300

    utilizando o modulo os para pega o nome do usuario do sistema e apresentando com template string.

--- notes-node/app.js
+++ notes-node/app.js
@@ -1,16 +1,8 @@
 console.log('Starting app')
 
 const fs = require('fs')
+const os = require('os')
 
-//original line
-fs.appendFile('greetings.txt', 'Hello world!')
+let user = os.userInfo()
 
-//Option one
-fs.appendFile('greetings.txt', 'Hello world!', function err(){
-  if(err){
-    console.log('Unable to write to file')
-  }
-})
-
-//option two
-fs.appendFileSync('greetings.txt', 'Hello World!')
+fs.appendFileSync('greetings.txt', `Hello ${user.username}`)
