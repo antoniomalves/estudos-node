2577ca5f2988b8582c50e598c0f196a7ef5d9279
Antonio M Alves Lima <antonio.malves.lima@gmail.com>
2018-08-04 12:35:53 -0300

    lendo a entrada do usuario atraves do comando process argv.

--- notes-node/app.js
+++ notes-node/app.js
@@ -4,3 +4,18 @@ const fs = require('fs')
 const _ = require('lodash')
 
 const notes = require('./notes')
+
+let command = process.argv[2]
+console.log('Command: ', command)
+
+if(_.isEqual('add', command)){
+  console.log('Adding new note')
+}else if(_.isEqual('list', command)){
+  console.log('Listing all notes')
+}else if(_.isEqual('read', command)){
+  console.log('Reading note')
+}else if(_.isEqual('remove', command)){
+  console.log('Removing note')
+}else{
+  console.log('Command not found')
+}
