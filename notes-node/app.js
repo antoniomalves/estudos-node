aeee86962bcee8dbc6e1a8a90869d55bb8284288
Antonio M Alves Lima <antonio.malves.lima@gmail.com>
2018-08-04 20:30:27 -0300

    criando no modulo notes funcionalidades para adicionar, ler e remover notas.

--- notes-node/app.js
+++ notes-node/app.js
@@ -2,20 +2,27 @@ console.log('Starting app')
 
 const fs = require('fs')  
 const _ = require('lodash')
+const yargs = require('yargs')
 
 const notes = require('./notes')
 
-let command = process.argv[2]
+const argv = yargs.argv
+//let command = process.argv[2]
+let command = argv._[0]
 console.log('Command: ', command)
+console.log('Yargs', argv)
 
 if(_.isEqual('add', command)){
-  console.log('Adding new note')
+  //node app.js add --title=secret --body="This is my secret"
+  notes.addNote(argv.title, argv.body)
 }else if(_.isEqual('list', command)){
-  console.log('Listing all notes')
+  //node app.js list
+  notes.getAll()
 }else if(_.isEqual('read', command)){
-  console.log('Reading note')
+  //node app.js read --title "secret"
+  notes.getNote(argv.title)
 }else if(_.isEqual('remove', command)){
-  console.log('Removing note')
+  notes.removeNote(argv.title)
 }else{
   console.log('Command not found')
 }
