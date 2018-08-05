6ded3113a6c2f643a2e2e55438bb926d6fecf45b
Antonio M Alves Lima <antonio.malves.lima@gmail.com>
2018-08-05 18:42:54 -0300

    Incluindo a funcionalidade de adicionar nota.

--- notes-node/notes.js
+++ notes-node/notes.js
@@ -1,7 +1,31 @@
 console.log('Starting notes.js')
 
+const fs = require('fs')
+const _ = require('lodash')
+
 let addNote = (title, body) => {
-  console.log('Adding note', title, body)
+  //console.log('Adding note', title, body)
+  let notes = [];
+  let note = {
+    title, 
+    body,
+  }
+
+  try {
+    let notesString = fs.readFileSync('notes-data.json')
+    notes = JSON.parse(notesString)  
+  } catch (error) {
+    
+  }
+
+  let duplicateNotes = notes.filter((note) => note.title === title); 
+
+  if(duplicateNotes.length === 0){
+    notes.push(note)
+    fs.writeFileSync('notes-data.json', JSON.stringify(notes))
+  }
+
+  console.log(duplicateNotes)
 }
 
 let getAll =() =>{
